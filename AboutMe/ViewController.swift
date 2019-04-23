688ea7ec2b84fbc33b6b81291c48558f50eea229
Taylor Lyles <hawks93187@gmail.com>
2019-04-23 09:38:47 -0700

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Taylor Lyles on 4/23/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("My name is Taylor")
+    }
+
+
+}
+
